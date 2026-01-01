3bbd424a4a044c6496562e73f59bb8d48b1eefb7
Supratim Mandal <supratimm98@gmail.com>
2026-01-01 14:23:41 +0530

    Change CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());    
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://api-chat-app-n5rw.onrender.com',
     credentials: true,  
 }));
 
