765241bc4f57711f70111349180c86488cff128a
Supratim Mandal <supratimm98@gmail.com>
2026-01-01 14:25:30 +0530

    Update CORS origin for chat application

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());    
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-    origin: 'https://api-chat-app-n5rw.onrender.com',
+    origin: 'https://realtime-chat-application-seven-tau.vercel.app/',
     credentials: true,  
 }));
 
