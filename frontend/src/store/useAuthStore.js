e974b28527d1f428b8e2b39f141a8bd87b3bf109
Supratim Mandal <supratimm98@gmail.com>
2026-01-01 14:49:18 +0530

    Update BASE_URL to production endpoint

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://api-chat-app-n5rw.onrender.com";
 
 export const userAuthStore = create((set, get) => ({
   authUser: null,
